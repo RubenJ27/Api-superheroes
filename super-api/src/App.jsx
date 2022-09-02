3cfc95cc3831b5623ed90372aede3d9d7385413d
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-09-01 23:49:18 -0500

    Se termino de agregar las imagenes de los heroes y la estructura de la api

--- super-api/src/App.jsx
+++ super-api/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [heroes, setHeroes] = useState([]);
+
+  useEffect(() => {
+    llamarNuestraApi();
+  }, [])
+  
+
+  const llamarNuestraApi = async () => {
+    const response = await fetch('http://localhost:4000/superheroes');
+    const datos = await response.json();
+    setHeroes(datos);
+  };
+
 
   return (
     <div className="App">
-      <h1>Hola API</h1>
+      <div>
+        {heroes.map((heroe) => (
+          <div key={heroe.id}>
+            <h3>{heroe.nombre}</h3>
+            <p>{heroe.editorial}</p>
+            <img className='img__heroes' src={heroe.imagen} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
